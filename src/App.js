0311ab0081b7ba3fb3c47e5c5f8a96acaba04b2d
John Manack <jmnck28@gmail.com>
2021-03-04 14:25:04 -0500

    establishes routes; creates characterprofile and characterlist components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,45 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Switch } from 'react-router-dom';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Star Wars React</h1>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      characters: [],
+    }
+  }
+  
+  _loadData = async() => {
+    const url = 'https://swapi.dev/api/people/';
+    const data = await fetch(url).then(response => response.json());
+    return data;
+  }
+
+  async componentDidMount() {
+    const apiData = await this._loadData();
+    this.setState({
+      characters: apiData
+    })
+    console.log('Characters: ', this.state.characters)
+  }
+
+  render () {
+    return (
+      <Router>
+        <div className="App">
+          <header className="App-header">
+            <h1>Star Wars API - React</h1>
+            <nav>
+            </nav>
+          </header>
+        </div>
+        <Switch>
+          {/* Place component Here */}
+        </Switch>
+      </Router>
+    )
+  }
 }
 
 export default App;
